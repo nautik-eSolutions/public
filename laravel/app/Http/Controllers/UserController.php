601b1b01caee5c151364d481f69b56c7ce2821f8
mlemrissani <mlemrissani1056@alumnes.politecnicllevant.cat>
2026-01-21 08:52:17 +0100

    Creación del método update y del ruta para consultar un usuario de la base de datos

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -13,7 +14,7 @@ class UserController extends Controller
     public function index()
     {
         $users = User::all();
-        return response()->json($users,200);
+        return response()->json($users, 200);
     }
 
     /**
@@ -22,36 +23,80 @@ class UserController extends Controller
     public function store(Request $request)
     {
         $user = User::create([
-            'first_name'=>$request->firstName,
-            'last_name'=>$request->lastName,
-            'user_name'=>$request->userName,
-            'email'=>$request->email,
-            'password'=>$request->password
+            'first_name' => $request->firstName,
+            'last_name' => $request->lastName,
+            'user_name' => $request->userName,
+            'email' => $request->email,
+            'password' => $request->password
         ]);
-        return response()->json($user,201);
+        return response()->json($user, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $username)
     {
-        //
+        $user = User::where('user_name', $username)->get();
+        return response()->json($user, 200);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $username)
     {
-        //
+        $user = User::where('user_name', $username)->get();
+        if (!$user) {
+            $data = [
+                'message' => "User not found",
+                'status' => 404
+            ];
+
+            return response()->json($data, 404);
+
+        }
+        $validator = Validator::make($request->all(), [
+            "firstName"=>'required',
+            "lastName"=>'required',
+            "userName"=>'required',
+            "email"=>'required|email',
+            "password"=>'required'
+        ]);
+
+        if ($validator->fails()){
+            $data = [
+                'message'=>'Error in data validation',
+                'errors'=>$validator->errors(),
+                'status'=>400
+            ];
+            return response()->json($data,400);
+        }
+        $user->firstName =  $request->firstname;
+        $user->lastName = $request->lastName;
+        $user->userName = $request->userName;
+        $user->email = $request->email;
+        $user->password = $request->password;
+
+        $user->save();
+
+
+        $data = [
+            'user'=>$user,
+            'status'=>200
+        ];
+
+        return response()->json($data,200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $username)
     {
-        //
+        $user = User::where('user_name', $username)->get();
+
+        $user->delete();
+
     }
 }
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -4,7 +4,5 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-Route::post('users',[UserController::class,'store']);
+Route::get('/users/{username}', [UserController::class,'show']);
+Route::post('/users',[UserController::class,'store']);
