a4c7707bb6d6025d54b3d2af804732d1dc7201ca
mlemrissani <mlemrissani1056@alumnes.politecnicllevant.cat>
2026-02-02 08:34:31 +0100

    commit before checkout

--- vue/src/main.js
+++ vue/src/main.js
@@ -5,9 +5,11 @@ import App from './App.vue'
 import router from './router/index.js'
 import axiosInstance from '@/plugins/axios.js'
 import PrimeVue from 'primevue/config'
+import { definePreset } from '@primeuix/themes'
 const app = createApp(App)
-
 app.config.globalProperties.$axios = axiosInstance;
+
+
 
 app.use(router)
 app.use(PrimeVue)
