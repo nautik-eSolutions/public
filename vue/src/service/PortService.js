1e5157f20918e84aa42ed61d98d5220f8c031195
mlemrissani <mlemrissani1056@alumnes.politecnicllevant.cat>
2026-02-11 17:11:41 +0100

    Implementación del store para extraer puertos

--- vue/src/service/PortService.js
+++ vue/src/service/PortService.js
@@ -1,27 +1,20 @@
 import { Port } from '@/model/Port.js'
 import axiosInstance from '@/plugins/axios.js'
 
-export class PortService {
-  static async getPorts() {
-    const {data}= await axiosInstance.get("ports").
-    then(resp => resp.data)
 
-    return data.ports.map(port =>this.#fromJson(port))
-
+  export  async function getPorts() {
+    const resp = await axiosInstance.get("ports");
+    return resp
   }
 
-
-
-
-
-  static #fromJson(rawData){
+  function #fromJson(rawData){
     return new Port(
       rawData.id,
       rawData.name,
       rawData.city,
       rawData.zones
     );
-  }
+
 
 
 }
---
+++ vue/src/stores/portsStore.js
@@ -0,0 +1,30 @@
+import { defineStore } from 'pinia'
+import router from '@/router/index.js'
+import { getPorts } from '@/service/PortService.js'
+import { Port } from '@/model/Port.js'
+
+export const usePortsStore = defineStore('portStore', {
+  state: () => {
+    return {
+      Ports:[]
+    }
+  },
+  actions: {
+    async getPorts(){
+      const resp =  await getPorts()
+      if (resp.status !== 200){
+        return  [];
+      } else {
+        return resp.data.ports.map(port=> this.#fromJson(port))
+      }
+    },
+    #fromJson(rawData){
+      return new Port(
+        rawData.id,
+        rawData.name,
+        rawData.city,
+        rawData.zones
+      );
+    }
+  },
+})
