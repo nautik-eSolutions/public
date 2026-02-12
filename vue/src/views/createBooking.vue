e147402888afe5eb54b30cf6d07e0cc35eb63447
mlemrissani <mlemrissani1056@alumnes.politecnicllevant.cat>
2026-02-12 22:47:48 +0100

    Completar formulario

--- vue/src/views/createBooking.vue
+++ vue/src/views/createBooking.vue
@@ -15,8 +15,8 @@ import Splitter from '@/volt/Splitter.vue'
           <p class="py-2 px-4 text-3xl">Completar reserva</p>
         </div>
         <div class="w-full flex gap-2 rounded-md">
-          <div class="border border-gray-300 rounded-lg shadow-lg w-3/5">
-            <div class="py-2 px-4">
+          <div class="border border-gray-300 py-5 px-3 rounded-lg shadow-lg w-3/5">
+            <div class="py- px-4">
               <h1 class="text-xl">Dirección de facturación</h1>
             </div>
             <div class="p-4 flex flex-col gap-6">
@@ -35,18 +35,58 @@ import Splitter from '@/volt/Splitter.vue'
                 </div>
               </div>
             </div>
+            <div class="py-2 px-4">
+              <h1 class="text-xl">Información de contacto</h1>
+              <div class="flex flex-col">
+                <label for="email">Correo electrónico</label>
+                <InputText id="email" />
+              </div>
+              <div class="flex flex-col w-1/2">
+                <label for="email">Telefóno</label>
+                <InputText id="email" />
+              </div>
+            </div>
           </div>
           <div class="border border-gray-300 rounded-lg shadow-lg w-2/5">
             <div class="p-4 flex flex-col gap-2">
               <h1 class="text-2xl text-center">Real club náutico de palma</h1>
               <Splitter />
-              <div class="flex flex-col p-2">
-                <h1 class="text-lg">Zona</h1>
-                <h1 class="text-lg">Dimensiones</h1>
-                <h1 class="text-lg">Llegada</h1>
-                <h1 class="text-lg">Salida</h1>
-                <h1 class="text-lg">Embarcación</h1>
+              <div class="flex justify-between p-2">
+                <div class="flex flex-col text-sm">
+                  <h1 class="">Zona</h1>
+                  <h1 class="">Dimensiones</h1>
+                  <h1 class="">Llegada</h1>
+                  <h1 class="">Salida</h1>
+                  <h1 class="">Embarcación</h1>
+                </div>
+                <div class="flex flex-col text-sm text-right">
+                  <h1 class="">Lujo relajado</h1>
+                  <h1 class="">12m x 6m x 2m</h1>
+                  <h1 class="">12/06/2026</h1>
+                  <h1 class="">14/06/2026</h1>
+                  <h1 class="">Sotavent</h1>
+                </div>
               </div>
+              <Splitter />
+              <div class="flex justify-between p-2">
+                <div class="flex flex-col text-sm">
+                  <h1 class="">Precio amarre</h1>
+                  <h1 class="">Servicios</h1>
+                  <h1 class="">Comisión de reserva</h1>
+                  <h1 class="">I.V.A</h1>
+                  <h1 class="">Total</h1>
+                </div>
+                <div class="flex flex-col text-sm text-right">
+                  <h1 class="">425,00 €</h1>
+                  <h1 class="">45,00 €</h1>
+                  <h1 class="">10,00 €</h1>
+                  <h1 class="">100,80 €</h1>
+                  <h1 class="">580,80 €</h1>
+                </div>
+              </div>
+              <button class="bg-principal-blue text-white rounded-md shadow-lg p-2 text-center">
+                Proceder al pago
+              </button>
             </div>
           </div>
         </div>
