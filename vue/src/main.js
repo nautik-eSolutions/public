29e6861f2ab3324af5c85b60e8b0d382c01eb79b
Pedro <prubio82@alumnes.politecnicllevant.cat>
2026-02-14 17:49:43 +0100

    -

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,5 +1,4 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
@@ -8,6 +7,25 @@ import PrimeVue from 'primevue/config'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { useAuthStore } from '@/stores/authStore.js'
+import { createI18n } from 'vue-i18n'
+
+
+const i18n = createI18n({
+  locale: 'ja',
+  fallbackLocale: 'en',
+  messages: {
+    en: {
+      message: {
+        hello: 'hello world'
+      }
+    },
+    ja: {
+      message: {
+        hello: 'こんにちは、世界'
+      }
+    }
+  }
+})
 
 const pinia = createPinia()
 const app = createApp(App)
@@ -15,6 +33,7 @@ const app = createApp(App)
 pinia.use(piniaPluginPersistedstate)
 
 app.use(pinia)
+app.use(i18n)
 
 app.config.globalProperties.$axios = axiosInstance;
 
