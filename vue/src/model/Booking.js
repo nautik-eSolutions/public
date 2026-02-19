03883ea5ae532319c9e8f3d08aa2c1946ff662b5
Pedro <prubio82@alumnes.politecnicllevant.cat>
2026-02-19 12:11:37 +0100

    -

--- vue/src/model/Booking.js
+++ vue/src/model/Booking.js
@@ -1,8 +1,13 @@
 export class Booking{
-  constructor(PortName, startDate, endDate, totalCost) {
+  constructor(PortName, startDate, endDate, totalCost, boatId) {
     this.PortName=PortName;
     this.startDate=startDate;
     this.endDate =endDate;
     this.totalCost = totalCost;
+    this.boatId = boatId;
+  }
+
+  fromJson(json){
+
   }
 }
