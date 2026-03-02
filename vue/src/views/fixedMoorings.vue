691ddae0944d650821e76de9f3055e5f5358dcad
mlemrissani <mlemrissani1056@alumnes.politecnicllevant.cat>
2026-03-02 14:35:05 +0100

    Implementación de para mostrar las solicitudes de amarre fijo

--- vue/src/views/fixedMoorings.vue
+++ vue/src/views/fixedMoorings.vue
@@ -1,18 +1,26 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, shallowRef } from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
 import Header from '../components/general/header.vue'
 import Footer from '../components/general/footer.vue'
 import { usePortsStore } from '@/stores/portsStore.js'
 import AutoComplete from '@/volt/AutoComplete.vue'
+import { useFixedMooringStore } from '@/stores/fixedMooringStore.js'
+import { defineForm, field } from 'vue-yup-form'
 
 const showModal = ref(false)
-const formData = ref({
-  port: '',
-  mooringNumber: '',
+const generateForm = defineForm({
+  fields: {
+    port: field().required().number(),
+    mooringNumber: field().required().string().max(20).min(1),
+    observations: field().required().string().max(255).min(1),
+  },
 })
-const fixedMooringStorre =
+
+const form = shallowRef(generateForm())
+const submitted = ref(false)
+const fixedMooringStore = useFixedMooringStore();
 const portsStore = usePortsStore()
 const ports = ref()
 const filteredPorts = ref()
@@ -28,12 +36,23 @@ const searchPort = (event) => {
     }
   }, 250)
 }
+
+  
+
 onMounted(async () => {
   ports.value = await portsStore.getPorts()
 })
-const submitRequest = () => {
+async function submitRequest () {
   showModal.value = false
+  submitted.value = true
+  if(!isValid(form.value)){
 
+    return 
+  }else{
+    await fixedMooringStore.createFixedMooringRequest(form.value)
+  }
+  
+  
 }
 </script>
 <template>
@@ -56,7 +75,7 @@ const submitRequest = () => {
         </div>
       </div>
     </main>
-
+      
     <Footer />
 
     <Transition name="modal">
@@ -71,13 +90,19 @@ const submitRequest = () => {
           <form @submit.prevent="submitRequest" class="space-y-6">
             <div v-if="ports">
               <AutoComplete
-                v-model="formData.port"
+                v-model="form.port.$value"
                 optionLabel="name"
                 :suggestions="filteredPorts"
                 @complete="searchPort"
                 placeholder="Selecciona un puerto"
                 class="w-full"
               />
+              <span
+                  class="text-red-500 text-sm min-h-[20px]"
+                  v-if="submitted && form.port.$error"
+                >
+                  {{ form.port.$error.message }}
+                </span>
             </div>
 
             <div>
@@ -86,13 +111,37 @@ const submitRequest = () => {
               </label>
               <input
                 id="numeroAmarre"
-                v-model="formData.numeroAmarre"
+                v-model.trim="form.mooringNumber.$value"
                 type="text"
                 placeholder="Ej: A-125"
                 class="w-full px-4 py-2 border border-gray-200 rounded-lg outline-none"
                 required
               />
+              <span
+                  class="text-red-500 text-sm min-h-[20px]"
+                  v-if="submitted && form.mooringNumber.$error"
+                >
+                  {{ form.mooringNumber.$error.message }}
+                </span>
             </div>
+
+            <div>
+              <label for="observations" class="text-sm font-medium text-gray-700 mb-2">
+                Observaciones
+              </label>
+              <textarea
+                id="observations"
+                v-model="form.observations.$value"
+                placeholder="Escribe tus observaciones aquí"
+                class="w-full px-4 py-2 border border-gray-200 rounded-lg outline-none"
+              />
+              <span
+                  class="text-red-500 text-sm min-h-[20px]"
+                  v-if="submitted && form.observations.$error"
+                >
+                  {{ form.observations.$error.message }}
+                </span>
+              </div>  
 
             <div class="flex gap-3 pt-4">
               <button
