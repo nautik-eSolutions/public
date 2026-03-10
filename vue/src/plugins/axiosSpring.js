bb08f0287fb976abf45ff4869481ebf2f9467234
mlemrissani <mlemrissani1056@alumnes.politecnicllevant.cat>
2026-03-10 18:16:42 +0100

    Canvi en les url de Spring y Laravel

--- vue/src/plugins/axiosSpring.js
+++ vue/src/plugins/axiosSpring.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { auth } from '@/main.js'
 
 const axiosSpring = axios.create({
-  baseURL: 'import.meta.env.VITE_ADMIN_URL',
+  baseURL: import.meta.env.VITE_ADMIN_URL,
 })
 axiosSpring.interceptors.request.use((config) => {
   const token = auth.springToken
